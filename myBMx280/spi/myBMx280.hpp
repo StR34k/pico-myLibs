a3881d901f6bae3530377a262c4931225b8bab94
StR34k <pnearing@gmail.com>
2022-06-14 12:26:01 -0400

    Change calibration reading

--- myBMx280/spi/myBMx280.hpp
+++ myBMx280/spi/myBMx280.hpp
@@ -18,10 +18,26 @@ class myBMx280 {
 		static const uint8_t REG_CTRL_MEAS_ADDR		= 0XF4;	// RETURNS UINT8
 		static const uint8_t REG_CONFIG_ADDR		= 0XF5;	// RETURNS UINT8
 		// Temperature calibration registers:
-		static const uint8_t REG_TEMP_DIG_START_ADDR	= 0x88;
-		static const uint8_t REG_TEMP_DIG_LEN			= 6;
+		static const uint8_t REG_TEMP_DIG_T1_ADDR	= 0X88;	// UINT16
+		static const uint8_t REG_TEMP_DIG_T2_ADDR	= 0X8A;	// INT16
+		static const uint8_t REG_TEMP_DIG_T3_ADDR	= 0X8C;	// INT16
 		// Pressure calibration registers:
+		static const uint8_t REG_PRES_DIG_P1_ADDR	= 0X8E;	// UINT16
+		static const uint8_t REG_PRES_DIG_P2_ADDR	= 0X90;	// INT16
+		static const uint8_t REG_PRES_DIG_P3_ADDR	= 0X92;	// INT16
+		static const uint8_t REG_PRES_DIG_P4_ADDR	= 0X94;	// INT16
+		static const uint8_t REG_PRES_DIG_P5_ADDR	= 0X96;	// INT16
+		static const uint8_t REG_PRES_DIG_P6_ADDR	= 0X98;	// INT16
+		static const uint8_t REG_PRES_DIG_P7_ADDR	= 0X9A;	// INT16
+		static const uint8_t REG_PRES_DIG_P8_ADDR	= 0X9C;	// INT16
+		static const uint8_t REG_PRES_DIG_P9_ADDR	= 0X9E;	// INT16
 		// Humidity calibration registers:
+		static const uint8_t REG_HUM_DIG_H1_ADDR	= 0XA1;	// UINT8
+		static const uint8_t REG_HUM_DIG_H2_ADDR	= 0XE1;	// INT16
+		static const uint8_t REG_HUM_DIG_H3_ADDR	= 0XE3;	// UINT8
+		static const uint8_t REG_HUM_DIG_H4_ADDR	= 0XE4;	// INT16 * REGISTER E5 CONTAINS DATA FOR H5, TREAT SPECIAL
+		static const uint8_t REG_HUM_DIG_H5_ADDR	= 0XE5;	// INT16 * REGISTER E5 CONTAINS DATA FOR H4, TREAT SPECIAL.
+		static const uint8_t REG_HUM_DIG_H6_ADDR	= 0XE7;	// INT8
 		// Chipsets:
 		static const uint8_t CHIPMODEL_BMP280 = 0x58; // No humidity.
 		static const uint8_t CHIPMODEL_BME280 = 0x60; // Has humidity.
@@ -29,6 +45,12 @@ class myBMx280 {
 		static const uint8_t DEFAULT_CONFIG 		= 0b10000000;// Default config, Standby 500us, filter off, spi3wire off.
 		static const uint8_t DEFAULT_HUM_SETTINGS 	= 0b00000001;// Default humidity settings, hum OSR x1.
 		static const uint8_t DEFAULT_MEAS_SETTINGS	= 0b00100101;// Default settings, temp OSR x1, pres OSR x1, forced mode.
+	/* Public variables: */
+		bool have_humidity = false;
+		float temperature;
+		float pressure;
+		float humidity;
+
 
 	/* Public functions: */
 		myBMx280(spi_inst_t *spiObj, uint8_t csPin, uint8_t clkPin, uint8_t misoPin, uint8_t mosiPin);
@@ -43,8 +65,6 @@ class myBMx280 {
 		uint8_t _sckPin;
 		uint8_t _misoPin;
 		uint8_t _mosiPin;
-		// state variables:
-		bool _have_humidity = false;
 
 		// Calibration variables:
 		uint16_t	_dig_t1;
@@ -57,7 +77,12 @@ class myBMx280 {
 		void __readRegisters__(const uint8_t reg, uint8_t *buffer, const uint8_t length);
 		void __writeRegister__(const uint8_t reg, const uint8_t value);
 		void __readTemperatureCalibration__();
-
+		void __readPressureCalibration__();
+		void __readHumidityCalibration__();
+		uint8_t __readUint8__(const uint8_t reg);
+		int8_t __readInt8__(const uint8_t reg);
+		uint16_t __readUint16__(const uint8_t reg);
+		int16_t __readInt16__(const uint8_t reg);
 };
 
 myBMx280::myBMx280(spi_inst_t *spiObj, uint8_t csPin, uint8_t sckPin, uint8_t misoPin, uint8_t mosiPin) {
@@ -89,20 +114,23 @@ bool myBMx280::initialize(const uint8_t config, const uint8_t measCtrl, const ui
 	uint8_t chip_id = readID();
 	switch (chip_id) {
 		case CHIPMODEL_BME280:
-			_have_humidity = true;
+			have_humidity = true;
 			break;
 		case CHIPMODEL_BMP280:
-			_have_humidity = false;
+			have_humidity = false;
 			break;
 		default:
 			return false;
 	}
 // Read Calibrations:
 	__readTemperatureCalibration__();
-
+	__readPressureCalibration__();
+	if (have_humidity == true) {
+		__readHumidityCalibration__();
+	}
 // Write configureation:
 	__writeRegister__(REG_CONFIG_ADDR, config);
-	if (_have_humidity == true) {
+	if (have_humidity == true) {
 		__writeRegister__(REG_CTRL_HUM_ADDR, humCtrl);
 	}
 	__writeRegister__(REG_CTRL_MEAS_ADDR, measCtrl);
@@ -133,12 +161,43 @@ void myBMx280::__writeRegister__(const uint8_t reg, const uint8_t value) {
 }
 
 void myBMx280::__readTemperatureCalibration__() {
-	uint8_t buffer[REG_TEMP_DIG_LEN];
-	__readRegisters__(REG_TEMP_DIG_START_ADDR, buffer, REG_TEMP_DIG_LEN);
-	_dig_t1 = ((uint16_t)buffer[1] << 8) | (uint16_t)buffer[0];
-	_dig_t2 = ((int16_t)buffer[3] << 8) | (int16_t)buffer[2];
-	_dig_t3 = ((int16_t)buffer[5] << 8) | (int16_t)buffer[4];
-	printf("t1: %i t2: %i t3: %i", _dig_t1, _dig_t2, _dig_t3);
-	
+	_dig_t1 = __readUint16__(REG_TEMP_DIG_T1_ADDR);
+	_dig_t2 = __readInt16__(REG_TEMP_DIG_T2_ADDR);
+	_dig_t3 = __readInt16__(REG_TEMP_DIG_T3_ADDR);
+	printf ("t1:%i t2:%i t3:%i", _dig_t1, _dig_t2, _dig_t3);
+	while (true) {;}
 }
+
+void myBMx280::__readPressureCalibration__() {
+
+}
+
+void myBMx280::__readHumidityCalibration__() {
+
+}
+
+uint8_t myBMx280::__readUint8__(const uint8_t reg) {
+	uint8_t value;
+	__readRegisters__(reg, &value, 1);
+	return value;
+}
+
+int8_t myBMx280::__readInt8__(const uint8_t reg) {
+	uint8_t value;
+	__readRegisters__(reg, &value, 1);
+	return (int8_t)value;
+}
+
+uint16_t myBMx280::__readUint16__(const uint8_t reg) {
+	uint8_t buffer[2];
+	__readRegisters__(reg, buffer, 2);
+	return (uint16_t)(((uint16_t)buffer[1] << 8) | (uint16_t)buffer[0]);
+}
+
+int16_t myBMx280::__readInt16__(const uint8_t reg) {
+	uint8_t buffer[2];
+	__readRegisters__(reg, buffer, 2);
+	return (int16_t)(((int16_t)buffer[1] << 8) | (int16_t)buffer[0]);
+}
+
 #endif
