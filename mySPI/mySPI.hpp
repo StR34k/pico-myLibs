c8bae41ea8bdfd55fae4e92454ed844f1722a609
StR34k <pnearing@gmail.com>
2022-06-29 11:16:27 -0400

    Create mySPI.hpp

---
+++ mySPI/mySPI.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file mySPI.hpp
+ * @author Peter Nearing(pnearing@protonmail.com)
+ * @brief SPI Helper functions.
+ * @version 0.1
+ * @date 2022-06-29
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef MY_SPI_H
+#define MY_SPI_H
+namespace mySPI {
+    
+};
+#endif
